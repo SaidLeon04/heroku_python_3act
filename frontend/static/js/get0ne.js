bb9226e9bfeddc3baf7b27cc1677b44ffdec0e35
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-11-04 14:12:36 -0600

    Create get0ne.js

---
+++ frontend/static/js/get0ne.js
@@ -0,0 +1,31 @@
+function getOne(email) {
+    const URL = "http://localhost:8000/contactos";
+    var request = new XMLHttpRequest;
+    request.open('GET',URL +"/" +email,true);
+    request.send();
+    request.onload = () => {
+        const response = request.responseText;
+        const json = JSON.parse(response);
+        console.log("response: " + response);
+        console.log("json: " + json);
+
+        const tbody_contactos = document.getElementById("tbody_contactos");
+
+        tbody_contactos.innerHTML = null;
+
+        var tr = document.createElement("tr");
+        var td_email = document.createElement("td");
+        var td_nombre = document.createElement("td");
+        var td_telefono = document.createElement("td");
+
+        td_email.innerHTML = json["email"];
+        td_nombre.innerHTML = json["nombre"];
+        td_telefono.innerHTML = json["telefono"];
+
+        tr.appendChild(td_email);
+        tr.appendChild(td_nombre);
+        tr.appendChild(td_telefono);
+
+        tbody_contactos.appendChild(tr);
+    };
+};
