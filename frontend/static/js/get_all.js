5081895855d4fef6cde3dc8dcebe32d731128a03
Said León <122801569+SaidLeon04@users.noreply.github.com>
2023-11-04 14:13:04 -0600

    Update get_all.js

--- frontend/static/js/get_all.js
+++ frontend/static/js/get_all.js
@@ -1,31 +1,38 @@
 function getAll(){
+    const URL = "http://localhost:8000/contactos";
+    //const URL = "ttps://shm-backend-105ae4e301e9.herokuapp.com/contactos";
     var request = new XMLHttpRequest;
-    request.open("GET", "http://localhost:8000/contactos");
-    
-    request.send()
+    request.open('GET',URL);
+    request.send();
+
     request.onload = (e) => {
         const response = request.responseText;
-        const json = JSON.parse(response)
-        console.log("response" + response) 
-        console.log("json" + json) 
-        console.log("status code" + request.status) 
-        console.log("Email" + json[0][["email"]]) 
-        console.log("Nombre" + json[0][["nombre"]]) 
-        console.log("Telefono" + json[0][["telefono"]]) 
+        const json = JSON.parse(response);
+        console.log("response: " + response);
+        console.log("json: " + json);
+        console.log("status_code: " + request.status);
 
-        const tbody_contactos = document.getElementById("tbody_contactos")
-        var tr = document.createElement("tr")
-        var td_email = document.createElement("td")
-        var td_nombre = document.createElement("td")
-        var td_telefono = document.createElement("td")
-        td_email.innerHTML =json[0]["email"]
-        td_nombre.innerHTML =json[0]["nombre"]
-        td_telefono.innerHTML =json[0]["telefono"]
+        console.log("Email: " + json[0]["email"]);
+        console.log("Nombre: " + json[0]["nombre"]);
+        console.log("Telefono: " + json[0]["telefono"]);
 
-        tr.appendChild(td_email)
-        tr.appendChild(td_nombre)
-        tr.appendChild(td_telefono)
+        const tbody_contactos = document.getElementById("tbody_contactos");
+        for (var i = 0; i < Object.keys(json).length; i++) {
+            var tr = document.createElement("tr");
+            var td_email = document.createElement("td");
+            var td_nombre = document.createElement("td");
+            var td_telefono = document.createElement("td");
 
-        tbody_contactos.appendChild(tr)
+            td_email.innerHTML = json[i]["email"];
+            td_nombre.innerHTML = json[i]["nombre"];
+            td_telefono.innerHTML = json[i]["telefono"];
+
+            console.log("Email: " + json[i]["email"]);
+
+            tr.appendChild(td_email);
+            tr.appendChild(td_nombre);
+            tr.appendChild(td_telefono);
+            tbody_contactos.appendChild(tr);
+        }
     };
 };
